9e9c84236aaf60caf7c0d5f286c346100128bc2c
Bruno Bacelar <39466847+TheDigitalPumpkin@users.noreply.github.com>
2019-05-20 21:17:06 -0300

    Add files via upload

---
+++ CharacterCreation/Background.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGproject.CharacterCreation
+{
+    class Background
+    {
+        private String Name { get; }
+        
+        public Background(String Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
